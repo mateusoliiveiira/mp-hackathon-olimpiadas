f9723f45f31670eee05f016886a5a02f7a840caf
Mateus <mateusoliiveira000@gmail.com>
2024-08-07 07:55:13 -0300

    as

---
+++ php/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/index.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <!---------Topo------------>
+    <header>
+        
+    <h1>Site Oficial De @OlimpiadasParis2024
+
+
+    </header>
+<!---------MEIO------------>
+    <main>
+
+    </main>
+
+    <!---------FIM----------->
+    <footer>
+
+    </footer>
+    <!--------------------------->
+
+</body>
+</html>
